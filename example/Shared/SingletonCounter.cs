4b47a555e2b6610bb87b9f328c2fd57e85f5644b
mitsugeek <mitsu322@gmail.com>
2020-06-17 01:24:32 +0900

    SingletonCounterを追加。ページをまたいでも保持されることを確認※F5だとダメ。

---
+++ example/Shared/SingletonCounter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace example.Shared
+{
+    public sealed class SingletonCounter
+    {
+        private static SingletonCounter _singleInstance = new SingletonCounter();
+
+        public static SingletonCounter GetInstance()
+        {
+            return _singleInstance;
+        }
+
+        public int currentCount { get; set; }
+
+        private SingletonCounter()
+        {
+            //initialization
+            currentCount = 0;
+        }
+    }
+}
